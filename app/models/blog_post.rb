b7b785646ad1d23bb0f02c36abb8878583b44cea
Grant Neale <gneale@mac.com>
2023-05-03 20:25:55 +0200

    stuff

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,4 +1,24 @@
 class BlogPost < ApplicationRecord
+    has_rich_text :body
     validates :title, presence: true
     validates :body, presence: true
+
+    # scope :sorted, -> { order(Arel.sql('published_at DESC NULLS LAST')).order(updated_at: :desc) }
+    scope :sorted, -> { order(arel_table[:published_at].desc.nulls_last).order(updated_at: :desc) }
+    scope :draft, -> { where(published_at: nil) }
+    scope :published, -> { where("published_at <= ?", Time.current) }
+    scope :scheduled, -> { where("published_at > ?", Time.current) }
+
+    def draft?
+        published_at.nil?        
+    end
+
+    def published?
+        # !draft?
+        published_at.present? && published_at <= Time.current
+    end 
+
+    def scheduled?
+        published_at.present? && published_at > Time.current
+    end 
 end
--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should return true for a draft blog post" do
+    #binding.irb
+    # assert blog_post = BlogPost.new(published_at: nil).draft?
+    assert blog_posts(:draft).draft?
+  end
+
+  test "draft ?should return false for a published blog post" do
+    #binding.irb
+    # refute blog_post = BlogPost.new(published_at: 1.year.ago).draft?
+    refute blog_posts(:published).draft?
+  end
+
+  test "draft ?should return false for a scheduled blog post" do
+    #binding.irb
+    # refute blog_post = BlogPost.new(published_at: 1.year.from_now).draft?
+    refute blog_posts(:scheduled).draft?
+  end
 end
